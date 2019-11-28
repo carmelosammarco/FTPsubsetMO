8c7ed4aa25020d9f0966679baf3f9e27a37aef3c
Carmelo <carmelosammarco@users.noreply.github.com>
2019-11-28 14:44:22 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf8") as fh:
     long_description = fh.read()
 
 setup(name='FTPsubsetMO',
-      version='0.0.8',
+      version='0.0.9',
       description='Python module able to download a file from FTP and subset it using bounding box and variable',
       long_description=long_description,
       long_description_content_type='text/markdown',
@@ -16,7 +16,9 @@ setup(name='FTPsubsetMO',
 
       install_requires=[
         'ftputil>=3.4',
-        'cdo>=1.4.0'
+        'netCDF4>=1.4.2', 
+        'pandas>=0.23.4', 
+        'xarray>=0.11.0'
 
 
       ],
